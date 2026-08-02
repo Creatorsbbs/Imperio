e725072e8b09edef9d5700e66df4cbbb85f56246
Creatorsbbs <serisvaldo927@gmail.com>
2026-08-02 16:10:15 -0300

    Implement slash command registration script
    This script reads command files from the './commands' directory, registers them as slash commands with Discord's API, and logs a success message.

---
+++ slash.js
@@ -0,0 +1,39 @@
+const {
+    REST,
+    Routes
+} = require("discord.js");
+
+const fs = require("fs");
+const path = require("path");
+
+const commands = [];
+
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(path.join(__dirname, "commands", file));
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({
+    version: "10"
+}).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            {
+                body: commands
+            }
+        );
+
+        console.log("Slash Commands registrados!");
+
+    } catch (err) {
+        console.error(err);
+    }
+})();
