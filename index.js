92ab771f26f8ec6c526ed7f97215f5222766f06c
Creatorsbbs <serisvaldo927@gmail.com>
2026-08-02 21:00:32 -0300

    Add dynamic event handler registration
    Load and register event handlers from the 'events' directory.

--- index.js
+++ index.js
@@ -52,6 +52,24 @@ for (const file of commandFiles) {
 
 }
 
+const eventsPath = path.join(__dirname, "events");
+
+const eventFiles = fs
+    .readdirSync(eventsPath)
+    .filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+
+    const event = require(path.join(eventsPath, file));
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+
+}
+
 client.once(Events.ClientReady, async readyClient => {
 
     console.log(`${readyClient.user.tag} está online!`);
