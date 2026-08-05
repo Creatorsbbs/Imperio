ddb0bf56712ed95bdbf6f2bc73ee9ca7f594a916
Creatorsbbs <serisvaldo927@gmail.com>
2026-08-05 13:26:18 -0300

    Update ponto.js

--- models/ponto.js
+++ models/ponto.js
@@ -1,1 +1,35 @@
 
+const mongoose = require("mongoose");
+
+const pontoSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    emServico: {
+        type: Boolean,
+        default: false
+    },
+
+    entrada: {
+        type: Date,
+        default: null
+    },
+
+    tempoTotal: {
+        type: Number,
+        default: 0
+    },
+
+    pontosBatidos: {
+        type: Number,
+        default: 0
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Ponto", pontoSchema);
