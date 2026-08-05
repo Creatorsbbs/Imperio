7f93eae91e33d26bbf8a2406638d8552a5dbc395
Creatorsbbs <serisvaldo927@gmail.com>
2026-08-05 14:10:00 -0300

    Update Limpardados.js

--- commands/Limpardados.js
+++ commands/Limpardados.js
@@ -1,10 +1,14 @@
 const {
     SlashCommandBuilder,
     EmbedBuilder,
-    PermissionFlagsBits
+    PermissionFlagsBits,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
 } = require("discord.js");
 
 const Ponto = require("../models/Ponto");
+
 
 module.exports = {
 
@@ -12,10 +16,17 @@ module.exports = {
 
         .setName("limpardados")
 
-        .setDescription("Apaga todos os registros de ponto.")
+        .setDescription("Apaga registros de ponto.")
 
         .setDefaultMemberPermissions(
             PermissionFlagsBits.Administrator
+        )
+
+        .addUserOption(option =>
+            option
+                .setName("usuario")
+                .setDescription("Usuário que terá os dados apagados.")
+                .setRequired(false)
         ),
 
 
@@ -27,31 +38,34 @@ module.exports = {
         )) {
 
             return interaction.reply({
-
                 content: "❌ Você não tem permissão para usar este comando.",
-
                 ephemeral: true
-
             });
 
         }
 
 
+        const usuario = interaction.options.getUser("usuario");
 
-        await Ponto.deleteMany({});
 
+        const alvo = usuario
+            ? `${usuario}`
+            : "👥 Todos os funcionários";
 
 
         const embed = new EmbedBuilder()
 
-            .setColor("#FF0000")
+            .setColor("#FFAA00")
 
-            .setTitle("🗑️ Dados de Ponto Apagados")
+            .setTitle("⚠️ Confirmação de limpeza")
 
             .setDescription(
-`Todos os registros de ponto foram removidos.
+`Você está prestes a apagar registros de ponto.
 
-👤 Responsável:
+🎯 **Alvo**
+${alvo}
+
+👤 **Responsável**
 ${interaction.user}
 
 ⚠️ Esta ação não pode ser desfeita.`
@@ -59,12 +73,44 @@ ${interaction.user}
 
 
 
+        const row = new ActionRowBuilder()
+
+            .addComponents(
+
+                new ButtonBuilder()
+
+                    .setCustomId(
+                        usuario
+                            ? `confirmar_limpeza_${usuario.id}`
+                            : "confirmar_limpeza_todos"
+                    )
+
+                    .setLabel("🗑️ Confirmar")
+
+                    .setStyle(ButtonStyle.Danger),
+
+
+                new ButtonBuilder()
+
+                    .setCustomId("cancelar_limpeza")
+
+                    .setLabel("❌ Cancelar")
+
+                    .setStyle(ButtonStyle.Secondary)
+
+            );
+
+
+
         await interaction.reply({
 
-            embeds: [embed]
+            embeds: [embed],
+
+            components: [row],
+
+            ephemeral: true
 
         });
-
 
     }
 
